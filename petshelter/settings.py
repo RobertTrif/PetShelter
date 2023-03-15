4c5dd1153f1bb50e228ed73de36e0e87284bc105
amarsalp <adriamarsalpons@gmail.com>
2023-03-16 00:06:02 +0100

    added models

--- petshelter/settings.py
+++ petshelter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'principal.apps.WebConfig'
+    'principal.apps.PrincipalConfig'
 ]
 
 MIDDLEWARE = [
--- principal/models.py
+++ principal/models.py
@@ -1,4 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Centro(models.Model):
+    nombre = models.CharField(max_length=256)
+    direccion = models.CharField(max_length=256)
 
+class Trabajador(models.Model):
+    nombre = models.CharField(max_length=256)
+    departamento = models.CharField(max_length=256)
+    centro = models.ForeignKey(Centro, on_delete=models.CASCADE)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=256)
+
+class Animal(models.Model):
+    peso = models.IntegerField()
+    color = models.CharField(max_length=256)
+
+class Adopcion(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, )
+    centro = models.ForeignKey(Centro, on_delete=models.CASCADE)
+    fecha = models.DateField()
+
+class Gato(Animal):
+    raza = models.CharField(max_length=256)
+    pelaje = models.CharField(max_length=256)
+
+class Perro(Animal):
+    raza = models.CharField(max_length=256)
+    pelaje = models.CharField(max_length=256)
+
+class Otro(Animal):
+    raza = models.CharField(max_length=256)
+    pelaje = models.CharField(max_length=256)
