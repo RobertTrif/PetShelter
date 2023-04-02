5b000f63d8a7ec571e89e9c432149138674d329b
ramraider67 <37593407+ramraider67@users.noreply.github.com>
2023-04-02 18:31:50 +0200

    base

--- petshelter/settings.py
+++ petshelter/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH_USER_MODEL = 'principal.WebUser'
+
+#LOGIN_REDIRECT_URL = 'home'
+#LOGOUT_REDIRECT_URL = 'home'
