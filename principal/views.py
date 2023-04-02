f1ef00d60f8fc92a7df7f73f482879a7ab637cc9
amarsalp <adriamarsalpons@gmail.com>
2023-04-02 17:34:46 +0200

    add new pages

--- principal/views.py
+++ principal/views.py
@@ -7,24 +7,10 @@ from principal.models import Centro, Trabajador, Cliente, Animal, Adopcion, Gato
 # Create your views here.
 # create a simple view index
 def index(request):
-    template = loader.get_template('index.html')
     animals = Animal.objects.all()
-    #context = {
-    #    'title': 'Adopciones',
-    #    'centros': Centro.objects.all(),
-    #    'trabajadores': Trabajador.objects.all(),
-    #    'clientes': Cliente.objects.all(),
-    #    'animales': Animal.objects.all(),
-    #    'adopciones': Adopcion.objects.all(),
-    #    'gatos': Gato.objects.all(),
-    #    'perros': Perro.objects.all(),
-    #    'otros': Otro.objects.all(),
-    #}
     return render(request, "index.html", {'animals': animals})
 
 # print information of specific animal
 def animal(request, animal_id):
     animal = Animal.objects.get(pk=animal_id)
     return render(request, "animal.html", {'animal': animal})
-
-hoasldjasldjlkasjdlksjdlkjslkdjlskjlkjlkjc
