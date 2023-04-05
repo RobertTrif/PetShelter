c7aae013198b38e75f3eb0b6943fff96016eac94
Cesar2306 <torcapolo2@gmail.com>
2023-04-05 18:53:36 +0200

    login v.2

--- principal/views.py
+++ principal/views.py
@@ -66,3 +66,17 @@ class RegistroTrabajadorView(CreateView):
         web_user = form.save()
         login(self.request, web_user)
         return redirect('Index')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro_cliente.html', {'form': form})
