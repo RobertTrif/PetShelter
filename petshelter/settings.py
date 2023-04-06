df4f79fcdb27b05dc6bbc928ad18515db101ac68
ramraider67 <37593407+ramraider67@users.noreply.github.com>
2023-04-06 18:41:09 +0200

    Login_logout users

--- petshelter/settings.py
+++ petshelter/settings.py
@@ -16,7 +16,7 @@ import os
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
