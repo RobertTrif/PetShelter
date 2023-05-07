8df53159dd9bf4d64d189f7b70abeb5eb6759cfc
bobbritel12 <britelyassine47@gmail.com>
2023-05-07 21:10:28 +0200

    Eliminacion
    Eliminacion

--- petshelter/urls.py
+++ petshelter/urls.py
@@ -16,6 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import principal.views as wv
+<<<<<<< Updated upstream
+=======
+import principal.forms as form
+from django.contrib.auth import views as auth_views
+from principal.views import CustomLoginView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+import principal.models as model
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.urls import path
+from principal.views import  eliminar_confirmar , eliminar
+>>>>>>> Stashed changes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +38,30 @@ urlpatterns = [
     path ('lista_animales/', wv.lista_animales, name='Lista_animales'),
     path ('centros/', wv.centros, name='Centros'),
     path ('centro/<int:centro_id>', wv.centro, name='Centro'),
+<<<<<<< Updated upstream
+=======
+    path ('registro_cliente/', wv.RegistroClienteView.as_view(), name='Registro_cliente'),
+    path ('registro_trabajador/', wv.RegistroTrabajadorView.as_view(), name='Registro_trabajador'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('administracion/', wv.administracion, name='Administracion'),
+    path('administracion/crear_perro/', 
+         CreateView.as_view(model=model.Perro, template_name = 'new_animal.html', form_class = form.NewPerro),
+         name='New_perro'),
+    path('administracion/crear_gato/', 
+         CreateView.as_view(model=model.Gato, template_name = 'new_animal.html', form_class = form.NewGato),
+         name='New_gato'),
+    path('administracion/crear_otro/', 
+         CreateView.as_view(model=model.Otro, template_name = 'new_animal.html', form_class = form.NewOtro),
+         name='New_otro'),
+    path('administracion/crear_centro/', 
+         CreateView.as_view(model=model.Centro, template_name = 'new_animal.html', form_class = form.NewCentro),
+         name='New_centro'),
+    path('administracion/<int:pk>', DetailView.as_view(model= model.Animal, template_name='animal_creado.html'), name='Animal_creado'),
+    
+    # Elliminacion de animales 
+    path('administracion/eliminar_animal/<str:animal_type>/', eliminar, name='eliminar'),
+    path('administracion/eliminar_animal/<str:animal_type>/<int:animal_id>/', wv.eliminar_confirmar, name='eliminar_confirmar'),
+>>>>>>> Stashed changes
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,8 +1,22 @@
+<<<<<<< Updated upstream
 from django.views import generic
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from principal.models import Centro, Trabajador, Cliente, Animal, Adopcion, Gato, Perro, Otro
+=======
+from django.http import Http404, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, get_user_model
+from django.contrib.auth.views import LoginView
+from .models import *
+from .forms import *
+
+>>>>>>> Stashed changes
 
 # Create your views here.
 # create a simple view index
@@ -39,3 +53,108 @@ def centro(request, centro_id):
     centro = Centro.objects.get(pk=centro_id)
     return render(request, "centro.html", {'centro':centro} )
 
+<<<<<<< Updated upstream
+=======
+def administracion(request):
+    return render(request, "administracion.html")
+
+class RegistroClienteView(CreateView):
+    model = WebUser
+    form_class = RegistroNuevoCliente
+    template_name = 'registro_cliente.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'cliente'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        web_user = form.save()
+        login(self.request, web_user)
+        return redirect('Index')
+    
+class RegistroTrabajadorView(CreateView):
+    model = WebUser
+    form_class = RegistroNuevoTrabajador
+    template_name = 'registro_trabajador.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'trabajador'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        web_user = form.save()
+        login(self.request, web_user)
+        return redirect('Index')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, 'Invalid username or password')
+    return render(request, 'users/login.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                self.add_error(None, 'Incorrect username or password. Please try again.')
+        return cleaned_data
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+# views.py
+def eliminar(request, animal_type):
+    if animal_type == "Perro":
+        AnimalModel = Perro
+    elif animal_type == "Gato":
+        AnimalModel = Gato
+    elif animal_type == "Otro":
+        AnimalModel = Otro
+    else:
+        AnimalModel = Animal
+
+    animales = AnimalModel.objects.all()
+
+    context = {"animales": animales, "animal_type": animal_type}
+    return render(request, "eliminar.html", context)
+
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import  Gato, Perro, Otro
+
+def eliminar_confirmar(request, animal_type, animal_id):
+    if animal_type == "Perro":
+        AnimalModel = Perro
+    elif animal_type == "Gato":
+        AnimalModel = Gato
+    elif animal_type == "Otro":
+        AnimalModel = Otro
+    else:
+        AnimalModel = Animal
+
+    animal = get_object_or_404(AnimalModel, pk=animal_id)
+    animal.delete()
+
+    messages.success(request, f"{animal.nombre} ha sido eliminado exitosamente.")
+
+    return redirect("eliminar", animal_type=animal_type)
+
+
+
+>>>>>>> Stashed changes
