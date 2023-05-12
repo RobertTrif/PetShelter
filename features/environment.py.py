e8a4d597ac790256d69e490db212fb8e4e5d73c8
amarsalp <adriamarsalpons@gmail.com>
2023-05-12 12:39:00 +0200

    add behave test 1 (doesn't work)

--- features/environment.py.py
+++
@@ -1,9 +0,0 @@
-from splinter.browser import Browser
-
-def before_all(context):
-    print("holaaaaaaaaaaaaaaaaaaaaaaa\n")
-    context.browser = Browser()
-
-def after_all(context):
-    context.browser.quit()
-    context.browser = None
