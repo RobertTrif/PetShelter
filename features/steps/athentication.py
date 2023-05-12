20b35e4a74e62ca3608f212404b9114cd1f2683a
amarsalp <adriamarsalpons@gmail.com>
2023-05-12 15:20:39 +0200

    behave login tests (works)

--- features/steps/athentication.py
+++ features/steps/athentication.py
@@ -7,10 +7,10 @@ def step_impl(context, username, password):
     from principal.models import Trabajador, WebUser, Centro
     centro = Centro.objects.create(nombre="centro", direccion="direccion")
     centro.save()
-    user = WebUser.objects.create(is_trabajador = True, password=password, username=username)
+    user = WebUser.objects.create_user(is_trabajador = True, password=password, username=username)
     user.save()
-    trabajador = Trabajador.objects.create(User=user, nombre="Pepe",apellidos="Marin",telefono=698524587,email="email@email.email",anos=93, departamento="departamento", adresa="adresa",
-            centro=centro)
+    trabajador = Trabajador.objects.create(User=user, nombre="Pepe",apellidos="Marin",telefono=698524587,email="email@email.email",anos=93, departamento="departamento", 
+                                           adresa="adresa", centro=centro)
     trabajador.save()
 
 @given('I login as worker "{username}" with password "{password}"')
@@ -18,15 +18,12 @@ def step_impl(context, username, password):
     from principal.models import WebUser, Trabajador
     user = WebUser.objects.get(username=username)
     trab = Trabajador.objects.get(User = user)
-    print("username:",trab.User.password,"\n")
-    print("pasus:",username,"\n")
-    print("pascon:",password,"\n")
     context.browser.visit(context.get_url('/login/'))
     form = context.browser.find_by_tag('form').first
     context.browser.fill('username', username)
     context.browser.fill('password', password)
     form.find_by_value('login').first.click()
-    assert context.browser.is_text_present('User: ' + username)
+    assert context.browser.is_text_present('Conectado como ' + trab.nombre + ' ' +trab.apellidos)
 '''
 @given('I login as user "{username}" with password "{password}"')
 def step_impl(context, username, password):
