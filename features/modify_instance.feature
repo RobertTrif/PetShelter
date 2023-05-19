f4dc9e227c755795aeb3489cbb6d3242b7421735
ramraider67 <37593407+ramraider67@users.noreply.github.com>
2023-05-19 13:24:02 +0200

    All_test_done

--- features/modify_instance.feature
+++ features/modify_instance.feature
@@ -13,10 +13,68 @@ Feature: Modify instances
         And I'm viewing the details of this center
             | nombre   | direccion  | 
             | centro_2 | direccio_2 |     
-        And go site and click the modify button
+        And Go site and click the modify centro button
         When I Modify a center
             | nombre   | direccion  | 
             | centro_Modificado2 | direccio_Modificado2 |
         And I'm viewing the details of this center
             | nombre   | direccion  | 
             | centro_Modificado2 | direccio_Modificado2 |
+
+    Scenario: Modify a cat
+        Given I login as worker "worker1_" with password "Hello67_"
+        When I register a "gato"
+            | nombre | peso | color   | raza    | pelaje | centro      |
+            | Michi  | 5    | Marron  | Abisino | corto  | centro_gato |
+        Then I'm viewing a message "Animal creado correctamente"
+        And I'm viewing the page details for this "Gato"
+            | nombre | peso | color   | raza    | pelaje | centro      |
+            | Michi  | 5    | Marron  | Abisino | corto  | centro_gato |        
+        And Go site and click the modify cat button
+        When I Modify a cat
+            | nombre | peso | color   | raza    | pelaje |
+            | MichiLini  | 15    | Marron  | Abisino | Largo  |
+        And I'm viewing the page details for this after modify "Gato"
+            | nombre | peso | color   | raza    | pelaje | centro      |
+            | MichiLini  | 15    | Marron  | Abisino | Largo  | centro_gato |  
+    
+    Scenario: Modify a dog
+        Given I login as worker "worker1_" with password "Hello67_"
+        When I register a "perro"
+            | nombre | peso | color | raza   | pelaje | centro       |
+            | Rufus  | 40   | Beix  | Golden | largo  | centro_perro |
+        Then I'm viewing a message "Animal creado correctamente"
+        And I'm viewing the page details for this "Perro"
+            | nombre | peso | color | raza   | pelaje | centro       |
+            | Rufus  | 40   | Beix  | Golden | largo  | centro_perro |
+        And Go site and click the modify dog button
+        When I modify a dog
+            | nombre | peso | color |
+            | RufusMix  | 20   | Negro  |
+         And I'm viewing the page details for this after modify "Perro"
+            | nombre | peso | color | raza   | pelaje | centro       |
+            | RufusMix  | 20   | Negro  | Golden | largo  | centro_perro |
+
+    Scenario: Modify a other
+        Given I login as worker "worker1_" with password "Hello67_"
+        When I register a "otro"
+            | nombre    | peso | color  | raza | pelaje | centro      |
+            | Sebastian | 500  | Marron | Oso  | Largo  | centro_otro |
+        Then I'm viewing a message "Animal creado correctamente"
+        And I'm viewing the page details for this "Otro"
+            | nombre    | peso | color  | raza | pelaje | centro      |
+            | Sebastian | 500  | Marron | Oso  | Largo  | centro_otro |
+        And Go site and click the modify other button
+        When I modify a other
+            | nombre    | peso | color  | raza | pelaje |
+            | Osito | 200  | Marron | Oso  | Largo  |
+        And I'm viewing the page details for this after modify "Otro"
+            | nombre    | peso | color  | raza | pelaje | centro      |
+            | Osito | 200  | Marron | Oso  | Largo  | centro_otro |
+    
+    Scenario: Modify a user
+        Given I login as worker "worker1_" with password "Hello67_"
+        And Go site and click the modify user button
+        When I modify a user
+            | nombre | apellidos | telefono | email |
+            | Osito | Amoroso | 123456777 | random@gmail.com |
--- features/steps/modify_instances.py
+++ features/steps/modify_instances.py
@@ -1,6 +1,6 @@
 from behave import *
 
-@step('Go site and click the modify button')
+@step('Go site and click the modify centro button')
 def step_impl(context):
     from principal.models import Centro
     centro_id = Centro.objects.get(nombre = "centro_2")
@@ -15,3 +15,97 @@ def step_impl(context):
         for heading in row.headings:
             context.browser.fill(heading, row[heading])
     form.find_by_value('Submit').first.click()
+
+@step('Go site and click the modify cat button')
+def step_impl(context):
+    from principal.models import Animal
+    animal_id = Animal.objects.get(nombre = "Michi")
+    
+    context.browser.visit(context.get_url('/animal/'+str(animal_id.pk)))
+    context.browser.links.find_by_text('Editar animal').click()
+
+@when('I Modify a cat')
+def step_impl(context):
+    from principal.models import Centro
+    centro = Centro.objects.create(nombre="centro_gato", direccion="direccion")
+    centro.save()
+    #fill the form
+    form = context.browser.find_by_tag('form').first
+    for row in context.table:
+        for heading in row.headings:
+            #if (heading != "centro"):
+                context.browser.fill(heading, row[heading])
+            #else:
+             #   context.browser.find_by_name('centro').select(str(centro.pk))
+    form.find_by_value('Submit').first.click()
+
+@step('Go site and click the modify dog button')
+def step_impl(context):
+    from principal.models import Animal
+    animal_id = Animal.objects.get(nombre = "Rufus")
+    
+    context.browser.visit(context.get_url('/animal/'+str(animal_id.pk)))
+    context.browser.links.find_by_text('Editar animal').click()
+
+@when('I Modify a dog')
+def step_impl(context):
+    from principal.models import Centro
+    centro = Centro.objects.create(nombre="centro_perro", direccion="direccion")
+    centro.save()
+    #fill the form
+    form = context.browser.find_by_tag('form').first
+    for row in context.table:
+        for heading in row.headings:
+            #if (heading != "centro"):
+                context.browser.fill(heading, row[heading])
+            #else:
+            #    context.browser.find_by_name('centro').select(str(centro.pk))
+        form.find_by_value('Submit').first.click()
+
+@step('Go site and click the modify other button')
+def step_impl(context):
+    from principal.models import Animal
+    animal_id = Animal.objects.get(nombre = "Sebastian")
+    
+    context.browser.visit(context.get_url('/animal/'+str(animal_id.pk)))
+    context.browser.links.find_by_text('Editar animal').click()
+
+@when('I Modify a other')
+def step_impl(context):
+    from principal.models import Centro
+    centro = Centro.objects.create(nombre="centro_otro", direccion="direccion")
+    centro.save()
+    #fill the form
+    form = context.browser.find_by_tag('form').first
+    for row in context.table:  
+        for heading in row.headings:
+            #if (heading != "centro"):
+                context.browser.fill(heading, row[heading])
+            #else:
+            #    context.browser.find_by_name('centro').select(str(centro.pk))
+        form.find_by_value('Submit').first.click()
+
+@step('Go site and click the modify user button')
+def step_impl(context):
+    context.browser.visit(context.get_url('/'))
+    context.browser.links.find_by_text('Usuario').click()
+
+@when('I Modify a user')
+def step_impl(context):
+    #fill the form
+    for row in context.table:
+        form = context.browser.find_by_tag('form').first
+        for heading in row.headings:
+            context.browser.fill(heading, row[heading])
+    form.find_by_value('Submit').first.click()
+
+@step('I\'m viewing the page details for this after modify "{animal}"')
+def step_impl(context, animal):
+    #assert that the animal was created correctly
+    table = context.table
+    assert context.browser.is_text_present('Nombre: ' + table[0][0])
+    assert context.browser.is_text_present('Peso (Kg): ' + table[0][1])
+    assert context.browser.is_text_present('Color: ' + table[0][2])
+    assert context.browser.is_text_present('Raza: ' + table[0][3])
+    assert context.browser.is_text_present('Pelaje: ' + table[0][4])
+    assert context.browser.is_text_present('Centro: ' + table[0][5])
